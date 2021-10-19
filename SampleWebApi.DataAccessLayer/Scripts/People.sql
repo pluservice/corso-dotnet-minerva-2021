1418ea6c81562351f48c37b746c5ccff8842e536
Marco Minerva <marco.minerva@gmail.com>
2021-10-19 12:59:19 +0200

    Add People Table SQL Script

---
+++ SampleWebApi.DataAccessLayer/Scripts/People.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[People](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](max) NULL,
+	[LastName] [nvarchar](max) NULL,
+	[City] [nvarchar](max) NULL,
+	[CreatedAt] [datetime] NULL,
+	[BirthDate] [datetime] NULL,
+ CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
